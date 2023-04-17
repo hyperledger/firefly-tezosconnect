bf4e613ef65d41383a20b74a0398e59fb03961aa
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-04-17 14:55:11 +0200

    DA-291 GetAddressBalance impl
    Signed-off-by: Dzianis Andreyenka <andreenkodn@gmail.com>

--- internal/tezos/get_address_balance.go
+++ internal/tezos/get_address_balance.go
@@ -3,10 +3,29 @@ package tezos
 import (
 	"context"
 
+	"blockwatch.cc/tzgo/tezos"
+	"github.com/hyperledger/firefly-common/pkg/fftypes"
 	"github.com/hyperledger/firefly-transaction-manager/pkg/ffcapi"
 )
 
 // AddressBalance gets the balance of the specified address
 func (c *tezosConnector) AddressBalance(ctx context.Context, req *ffcapi.AddressBalanceRequest) (*ffcapi.AddressBalanceResponse, ffcapi.ErrorReason, error) {
-	return nil, "", nil
+	addr, err := tezos.ParseAddress(req.Address)
+	if err != nil {
+		return nil, "Invalid address", err
+	}
+
+	headBlock, err := c.client.GetHeadBlock(ctx)
+	if err != nil {
+		return nil, "GetHeadBlock error", err
+	}
+
+	balance, err := c.client.GetContractBalance(ctx, addr, headBlock.Hash)
+	if err != nil {
+		return nil, "GetContractBalance error", err
+	}
+
+	return &ffcapi.AddressBalanceResponse{
+		Balance: (*fftypes.FFBigInt)(&balance),
+	}, "", nil
 }
