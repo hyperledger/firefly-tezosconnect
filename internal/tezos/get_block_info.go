6cf2b1a55a10bfb6274b3bfbcb67aba0fc0c095d
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-04-18 16:05:03 +0200

    DA-292 GetBlockInfo impl
    Signed-off-by: Dzianis Andreyenka <andreenkodn@gmail.com>

--- internal/tezos/get_block_info.go
+++ internal/tezos/get_block_info.go
@@ -3,6 +3,11 @@ package tezos
 import (
 	"context"
 
+	"blockwatch.cc/tzgo/rpc"
+	"blockwatch.cc/tzgo/tezos"
+	"github.com/OneOf-Inc/firefly-tezosconnect/internal/msgs"
+	"github.com/hyperledger/firefly-common/pkg/fftypes"
+	"github.com/hyperledger/firefly-common/pkg/i18n"
 	"github.com/hyperledger/firefly-transaction-manager/pkg/ffcapi"
 )
 
@@ -13,5 +18,44 @@ func (c *tezosConnector) BlockInfoByNumber(ctx context.Context, req *ffcapi.Bloc
 
 // BlockInfoByHash gets block information using the hash of the block
 func (c *tezosConnector) BlockInfoByHash(ctx context.Context, req *ffcapi.BlockInfoByHashRequest) (*ffcapi.BlockInfoByHashResponse, ffcapi.ErrorReason, error) {
-	return nil, "", nil
+	blockInfo, err := c.getBlockInfoByHash(ctx, req.BlockHash)
+	if err != nil {
+		return nil, ffcapi.ErrorReason(""), err
+	}
+	if blockInfo == nil {
+		return nil, ffcapi.ErrorReasonNotFound, i18n.NewError(ctx, msgs.MsgBlockNotAvailable)
+	}
+
+	res := &ffcapi.BlockInfoByHashResponse{}
+	transformBlockInfo(blockInfo, &res.BlockInfo)
+	return res, "", nil
+}
+
+func (c *tezosConnector) getBlockInfoByHash(ctx context.Context, hashString string) (*rpc.Block, error) {
+	var blockInfo *rpc.Block
+
+	blockHash, err := tezos.ParseBlockHash(hashString)
+	if err != nil {
+		return nil, err
+	}
+
+	blockInfo, err = c.client.GetBlock(ctx, blockHash)
+	if err != nil {
+		return nil, err
+	}
+
+	return blockInfo, nil
+}
+
+func transformBlockInfo(bi *rpc.Block, t *ffcapi.BlockInfo) {
+	t.BlockNumber = fftypes.NewFFBigInt(bi.Header.Level)
+	t.BlockHash = bi.Hash.String()
+	t.ParentHash = bi.Header.Predecessor.String()
+	stringHashes := make([]string, 0)
+	for _, batch := range bi.Operations {
+		for _, tx := range batch {
+			stringHashes = append(stringHashes, tx.Hash.String())
+		}
+	}
+	t.TransactionHashes = stringHashes
 }
