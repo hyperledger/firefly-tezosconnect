fcec4a6e780a6663142d710cc6f5ac2aa0928da1
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-04-27 18:01:55 +0200

    DA-328 nonce impl
    Signed-off-by: Dzianis Andreyenka <andreenkodn@gmail.com>

--- internal/tezos/get_next_nonce.go
+++ internal/tezos/get_next_nonce.go
@@ -3,10 +3,14 @@ package tezos
 import (
 	"context"
 
+	"github.com/hyperledger/firefly-common/pkg/fftypes"
 	"github.com/hyperledger/firefly-transaction-manager/pkg/ffcapi"
 )
 
-// NextNonceForSigner is used when there are no outstanding transactions for a given signing identity, to determine the next nonce to use for submission of a transaction
+// NextNonceForSigner. In Tezos, transactions are identified by their hash, which is unique for each transaction, so nonce is not necessary.
+// Return zero value for compatibility
 func (c *tezosConnector) NextNonceForSigner(ctx context.Context, req *ffcapi.NextNonceForSignerRequest) (*ffcapi.NextNonceForSignerResponse, ffcapi.ErrorReason, error) {
-	return nil, "", nil
+	return &ffcapi.NextNonceForSignerResponse{
+		Nonce: fftypes.NewFFBigInt(0),
+	}, "", nil
 }
