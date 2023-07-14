db07a663ce1683eb21b2c1f4285119b0049dcb1c
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-07-14 08:32:18 +0200

    DA-441 Add tx extra info into tx receipt
    Signed-off-by: Dzianis Andreyenka <andreenkodn@gmail.com>

--- internal/tezos/get_receipt.go
+++ internal/tezos/get_receipt.go
@@ -2,14 +2,32 @@ package tezos
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
+	"blockwatch.cc/tzgo/micheline"
 	"blockwatch.cc/tzgo/rpc"
 	"blockwatch.cc/tzgo/tezos"
 	"github.com/hyperledger/firefly-common/pkg/fftypes"
 	"github.com/hyperledger/firefly-common/pkg/log"
 	"github.com/hyperledger/firefly-transaction-manager/pkg/ffcapi"
 )
+
+type receiptExtraInfo struct {
+	ContractAddress     *tezos.Address    `json:"contractAddress"`
+	ConsumedGas         *fftypes.FFBigInt `json:"consumedGas"`
+	GasLimit            *fftypes.FFBigInt `json:"gasLimit"`
+	PaidStorageSizeDiff *fftypes.FFBigInt `json:"paidStorageSizeDiff"`
+	StorageSize         *fftypes.FFBigInt `json:"storageSize"`
+	StorageLimit        *fftypes.FFBigInt `json:"storageLimit"`
+	From                *tezos.Address    `json:"from"`
+	To                  *tezos.Address    `json:"to"`
+	Counter             *fftypes.FFBigInt `json:"counter"`
+	Fee                 *fftypes.FFBigInt `json:"fee"`
+	Status              *string           `json:"status"`
+	ErrorMessage        *string           `json:"errorMessage"`
+	Storage             *fftypes.JSONAny  `json:"storage"`
+}
 
 // TransactionReceipt queries to see if a receipt is available for a given transaction hash
 func (c *tezosConnector) TransactionReceipt(ctx context.Context, req *ffcapi.TransactionReceiptRequest) (*ffcapi.TransactionReceiptResponse, ffcapi.ErrorReason, error) {
@@ -46,15 +64,74 @@ func (c *tezosConnector) TransactionReceipt(ctx context.Context, req *ffcapi.Tra
 		blockNumber = block.BlockNumber.Int64()
 	}
 
-	resp := &ffcapi.TransactionReceiptResponse{
+	receiptResponse := &ffcapi.TransactionReceiptResponse{
 		BlockNumber:      fftypes.NewFFBigInt(blockNumber),
 		TransactionIndex: fftypes.NewFFBigInt(int64(receipt.Pos)),
 		BlockHash:        receipt.Block.String(),
 		Success:          receipt.IsSuccess(),
 		ProtocolID:       receipt.Op.Protocol.String(),
-		ExtraInfo:        nil,
-		ContractLocation: nil,
 	}
 
-	return resp, "", nil
+	if receipt.Op != nil {
+		var fullReceipt []byte
+		operationReceipts := make([]receiptExtraInfo, len(receipt.Op.Contents))
+
+		for _, o := range receipt.Op.Contents {
+			switch o.Kind() {
+			case tezos.OpTypeTransaction:
+				tx := o.(*rpc.Transaction)
+
+				txStatus := tx.Result().Status.String()
+				extraInfo := receiptExtraInfo{
+					ConsumedGas:         fftypes.NewFFBigInt(tx.Metadata.Result.ConsumedMilliGas / 1000),
+					GasLimit:            fftypes.NewFFBigInt(tx.GasLimit),
+					PaidStorageSizeDiff: fftypes.NewFFBigInt(tx.Metadata.Result.PaidStorageSizeDiff),
+					StorageSize:         fftypes.NewFFBigInt(tx.Metadata.Result.StorageSize),
+					StorageLimit:        fftypes.NewFFBigInt(tx.StorageLimit),
+					From:                &tx.Source,
+					To:                  &tx.Destination,
+					Counter:             fftypes.NewFFBigInt(tx.Counter),
+					Fee:                 fftypes.NewFFBigInt(tx.Fee),
+					Status:              &txStatus,
+				}
+
+				var script *micheline.Script
+				if tx.Destination.IsContract() {
+					location, _ := json.Marshal(map[string]string{
+						"address": tx.Destination.String(),
+					})
+					receiptResponse.ContractLocation = fftypes.JSONAnyPtrBytes(location)
+					extraInfo.ContractAddress = &tx.Destination
+
+					script, err = c.client.GetContractScript(ctx, tx.Destination)
+					if err != nil {
+						log.L(ctx).Error("error getting contract script: ", err)
+					}
+				}
+				if len(tx.Result().Errors) > 0 {
+					errorMessage := ""
+					for _, err := range tx.Result().Errors {
+						errorMessage += err.Error()
+					}
+					extraInfo.ErrorMessage = &errorMessage
+				}
+				if prim := tx.Metadata.Result.Storage; prim != nil {
+					val := micheline.NewValue(script.StorageType(), *prim)
+					m, err := val.Map()
+					if err != nil {
+						log.L(ctx).Error("error parsing contract storage: ", err)
+					}
+					storageBytes, _ := json.Marshal(m)
+					extraInfo.Storage = fftypes.JSONAnyPtrBytes(storageBytes)
+				}
+
+				operationReceipts = append(operationReceipts, extraInfo)
+				fullReceipt, _ = json.Marshal(&extraInfo)
+			}
+		}
+
+		receiptResponse.ExtraInfo = fftypes.JSONAnyPtrBytes(fullReceipt)
+	}
+
+	return receiptResponse, "", nil
 }
