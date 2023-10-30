078f67e1fb02c8ffa6001e897183aa712a4512d2
Alexey Semenyuk <alexsemenyuk88@gmail.com>
2023-10-30 15:39:02 +0600

    Increase code coverage
    Signed-off-by: Alexey Semenyuk <alexsemenyuk88@gmail.com>

--- internal/tezos/retry_delay_test.go
+++ internal/tezos/retry_delay_test.go
@@ -12,34 +12,43 @@ func TestRetryDelay(t *testing.T) {
 	_, c, _, done := newTestConnector(t)
 	defer done()
 
-	c.retry.MaximumDelay = 1 * time.Microsecond
-	c.retry.InitialDelay = 100 * time.Microsecond
+	c.retry.MaximumDelay = 100 * time.Microsecond
+	c.retry.InitialDelay = 1 * time.Microsecond
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		cancel()
+	}()
 
 	testCases := []struct {
 		name         string
 		failureCount int
+		ctx          context.Context
 		result       bool
 	}{
 		{
 			name:         "zero failure count",
 			failureCount: 0,
+			ctx:          context.Background(),
 			result:       false,
 		},
 		{
 			name:         "retry delay exceeds max delay",
 			failureCount: 10,
+			ctx:          context.Background(),
 			result:       false,
 		},
 		{
 			name:         "ctx done",
-			failureCount: 0,
-			result:       false,
+			failureCount: 10,
+			ctx:          ctx,
+			result:       true,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			res := c.doFailureDelay(context.Background(), 0)
+			res := c.doFailureDelay(tc.ctx, tc.failureCount)
 			assert.Equal(t, tc.result, res)
 		})
 	}
